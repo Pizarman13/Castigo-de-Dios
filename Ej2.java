e498413c7a77604afba30fe39ba9ce9f731e8ebf
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2024-02-07 20:18:50 +0100

    ej2

---
+++ Ej2.java
@@ -0,0 +1,10 @@
+public class Ej2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("El mayor número que se puede representar en un char es: " + (int)Character.MAX_VALUE);
+        System.out.println("El mayor número que se puede representar en un short es: " + Short.MAX_VALUE);
+        System.out.println("El mayor número que se puede representar en un int es: " + Integer.MAX_VALUE);
+    }
+
+}
